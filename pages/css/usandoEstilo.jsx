1acfae970c11cd8292558570d1a2d5c0b02d1ef6
Carlos Bekinho <carlos.dev.job@gmail.com>
2024-01-31 22:57:50 -0300

    aplicando CSS condicional

---
+++ pages/css/usandoEstilo.jsx
@@ -0,0 +1,10 @@
+import Estilo from "@/components/Estilo";
+
+export default function usandoTitulo() {
+    return (
+        <div>
+            <Estilo numero={3} color="#fff" />
+            <Estilo numero={-13} color="#000" direita/>
+        </div>
+    )
+}
